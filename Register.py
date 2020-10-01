642c3aa2bb7b282d0b9c826f76a9319099605259
vrnahuja <40837577+vrnahuja@users.noreply.github.com>
2020-10-01 16:02:08 +0530

    Update Register.py

--- Register.py
+++ Register.py
@@ -6,7 +6,7 @@ mydb=mysql.connector.connect(
         database="python")
 db=mydb.cursor()
 db.execute("select * from data")
-print("DATA:")
+print("DATA::")
 r=db.fetchall()
 for i in r: 
     print(i)
